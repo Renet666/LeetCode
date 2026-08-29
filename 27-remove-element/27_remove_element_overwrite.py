572b019a548f82f9b4e791cb62b5154095fec44b
rene <rene@beelinerouter.beeline>
2026-08-29 01:32:11 -0500

    Add two solutions 27-remove-element

---
+++ 27-remove-element/27_remove_element_overwrite.py
@@ -0,0 +1,8 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        writer = 0
+        for i in range(0, len(nums)):
+            if nums[i] != val:
+                nums[writer] = nums[i]
+                writer += 1
+        return writer
